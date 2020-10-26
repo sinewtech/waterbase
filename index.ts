b24b0c17c27f8151c937348abe1e88ec787d7362
David Mendoza <d.mendoza99@unitec.edu>
2020-10-26 16:20:29 -0600

    made private the client

--- index.ts
+++ index.ts
@@ -6,32 +6,30 @@ import { WaterbaseConfig } from './lib/models/Config';
 class Waterbase {
   private auth: Auth;
 
-  private Client: Client;
-
   private database: Database;
 
+  #Client: Client;
+
   constructor() {
-    this.Client = {} as Client;
+    this.#Client = {} as Client;
     this.auth = {} as Auth;
     this.database = {} as Database;
   }
 
   initialize = (config: WaterbaseConfig) => {
-    this.Client = new Client(config);
+    this.#Client = new Client(config);
   };
 
   Auth = (): Auth => {
     if (this.auth !== ({} as Auth)) {
-      console.log('se creo');
-      this.auth = new Auth(this.Client);
+      this.auth = new Auth(this.#Client);
     }
     return this.auth;
   };
 
   Database = (): Database => {
     if (this.database !== ({} as Database)) {
-      console.log('se creo');
-      this.database = new Database(this.Client);
+      this.database = new Database(this.#Client);
     }
     return this.database;
   };
