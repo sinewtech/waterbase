02aa7edf90a883ae9453f9c9a05f07143b780fd9
David Mendoza <d.mendoza99@unitec.edu>
2020-10-25 21:35:12 -0600

    comments and changed the _ for # in the variables not wanted

--- lib/helpers/Collection.ts
+++ lib/helpers/Collection.ts
@@ -1,52 +1,87 @@
-/* eslint-disable no-underscore-dangle */
 import Client from '../Client';
 import Document from './Document';
+import { Collection as Coll } from '../models/Collection';
 
-class Collection {
+class Collection implements Coll {
   name: string = '';
 
-  client: Client;
+  #client: Client;
 
-  query: object = {};
+  #query: object = {};
 
   constructor(client: Client, name: string) {
     this.name = name;
-    this.client = client;
+    this.#client = client;
   }
 
+  /**
+   * Assigns the query to be ready before getting the docs
+   *
+   * @param query object
+   */
   where = (query: object) => {
-    this.query = query;
+    this.#query = query;
     return this;
   };
 
+  /**
+   * Gets a single doc of the collection
+   *
+   * @param id string
+   */
+  doc = (id: string) =>
+    new Promise<Document>((res, rej) => {
+      this.#client
+        .call(
+          'post',
+          `/collections/query/${this.name}`,
+          {
+            'content-type': 'application/json',
+          },
+          { query: { _id: id } }
+        )
+        .then((value) => {
+          const { _id, ...docData } = value.docs[0];
+          res(new Document(this.#client, this.name, _id, docData));
+        })
+        .catch(rej);
+    });
+
+  /**
+   * Gets gets all the docs that the where got or all in general
+   *
+   * @param id string
+   */
   get = () =>
     new Promise<object>((res, rej) => {
-      if (this.name !== '' && this.query !== {}) {
-        this.client
+      if (this.name !== '' && this.#query !== {}) {
+        this.#client
           .call(
             'post',
             `/collections/query/${this.name}`,
             {
               'content-type': 'application/json',
             },
-            { query: this.query }
+            { query: this.#query }
           )
           .then((data) => {
             const Documents: Document[] = data.docs.map((doc: any) => {
               const { _id, ...docData } = doc;
-              return new Document(this.client, this.name, _id, docData);
+              return new Document(this.#client, this.name, _id, docData);
             });
+            this.#query = {};
             res(Documents);
           })
           .catch(rej);
-      } else if (this.name !== '' && this.query === {}) {
-        this.client
+      } else if (this.name !== '' && this.#query === {}) {
+        this.#client
           .call('get', `/collections/${this.name}`)
           .then((data) => {
             const Documents: Document[] = data.docs.map((doc: any) => {
               const { _id, ...docData } = doc;
-              return new Document(this.client, this.name, _id, docData);
+              return new Document(this.#client, this.name, _id, docData);
             });
+            this.#query = {};
             res(Documents);
           })
           .catch(rej);
