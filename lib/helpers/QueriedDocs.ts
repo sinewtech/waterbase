fd451937cb16c3e7281ca1177ec04c026c267efd
David Mendoza <d.mendoza99@unitec.edu>
2020-10-30 12:03:40 -0600

    continued the format

--- lib/helpers/QueriedDocs.ts
+++ lib/helpers/QueriedDocs.ts
@@ -3,8 +3,11 @@ import { Documents } from '../models/Documents';
 
 class QueriedDocs implements QD {
   size: number = 0;
+
   docs: Documents[] = [];
+
   empty: boolean = false;
+
   query: object = {};
 
   constructor(query: object, docs: Documents[]) {
