e72243a8d163643e42beb23b881ba953b87a7c02
David Mendoza <d.mendoza99@unitec.edu>
2020-11-03 18:22:58 -0600

    from File to file

--- lib/services/Storage.ts
+++ lib/services/Storage.ts
@@ -2,6 +2,6 @@ import Service from '../Service';
 import File from '../helpers/File';
 
 class Storage extends Service {
-  File = (path: string) => new File(this.client, path);
+  file = (path: string) => new File(this.client, path);
 }
 export = Storage;
